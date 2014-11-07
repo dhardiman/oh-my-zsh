3c88ca4e75469e7684e693abf26b3daf2a6e4ae0
David Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2014-11-07 15:14:03 +0000

    Fix merge conflict

--- lib/git.zsh
+++ lib/git.zsh
@@ -115,11 +115,10 @@ function git_prompt_info() {
   # ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   ref=$(__git_ps1 "%s") || return
 
-    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
 
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-  fi
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 
